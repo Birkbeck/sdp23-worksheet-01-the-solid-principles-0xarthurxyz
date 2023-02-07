2642b010475638aa0b67424013f27f778f9685a8
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-02-07 10:42:07 +0000

    Adds new `upgrade()` method (1) for `ApartmentWithRooms` and (2) for `Apartment`

--- 03_liskov_substitution/src/UnitUpgrader.java
+++ 03_liskov_substitution/src/UnitUpgrader.java
@@ -1,8 +1,10 @@
 public class UnitUpgrader {
+    public void upgrade(ApartmentWithRooms apartment) {
+        apartment.squareFootage += 40;
+        apartment.numberOfBedrooms += 1;
+    }
+    
     public void upgrade(Apartment apartment) {
         apartment.squareFootage += 40;
-
-        if (apartment.getClass() != Studio.class)
-            apartment.numberOfBedrooms += 1;
     }
 }
