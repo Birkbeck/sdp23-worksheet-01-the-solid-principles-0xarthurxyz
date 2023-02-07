2f798a7a6c9f4f05e862518a8c42f5d9a68c80f1
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-02-07 10:41:33 +0000

    Removes previously added constructor (not necessary) and adds constant attribute `numberOfBedrooms`

--- 03_liskov_substitution/src/Studio.java
+++ 03_liskov_substitution/src/Studio.java
@@ -1,7 +1,5 @@
 public final class Studio extends Apartment {
-    public Studio() {
-        this.numberOfBedrooms = 0;
-    }
+    int numberOfBedrooms = 0;
 
     public void setSquareFootage(int sqft) {
         this.squareFootage = sqft;
