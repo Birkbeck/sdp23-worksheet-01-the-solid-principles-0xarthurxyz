fb86b03674e0b63f256034c37c4c1c31a42ef5c2
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-02-07 11:11:12 +0000

    Refactor: Changes class definition (1) now extends Bird, (2) implements smaller interfaces, (3) adds class comments for readability

--- 04_interface_segregation/src/Eagle.java
+++ 04_interface_segregation/src/Eagle.java
@@ -1,11 +1,14 @@
-public class Eagle implements Bird {
+public class Eagle extends Bird implements FlyingAnimal, MoltingAnimal {
+    // Attribute(s)
     String currentLocation;
     int numberOfFeathers;
-
+    
+    // Constructor
     public Eagle(int initialFeatherCount) {
         this.numberOfFeathers = initialFeatherCount;
     }
 
+    // Interface methods
     @Override
     public void fly() {
         this.currentLocation = "in the air";
--- 04_interface_segregation/src/Penguin.java
+++ 04_interface_segregation/src/Penguin.java
@@ -1,22 +1,25 @@
-public class Penguin implements Bird {
+public class Penguin extends Bird implements SwimmingAnimal, MoltingAnimal {
+    // Attribute(s)
     String currentLocation;
     int numberOfFeathers;
 
+    // Constructor
     public Penguin(int initialFeatherCount) {
         this.numberOfFeathers = initialFeatherCount;
     }
 
+    // Interface methods
     @Override
     public void molt() {
         this.numberOfFeathers -= 1;
     }
 
-    @Override
-    public void fly() {
-        throw new UnsupportedOperationException();
-    }
-
     public void swim() {
         this.currentLocation = "in the water";
+    }
+
+    // Methods
+    public void fly() {
+        throw new UnsupportedOperationException();
     }
 }
